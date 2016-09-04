d59b0431887d518afa77008327f246850f451c64
mrmojica <mrmojica510@gmail.com>
2016-09-03 17:56:36 -0700

    sample

---
+++ app.js
@@ -0,0 +1,11 @@
+// Libs
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Components
+import Scoreboard from './components/Scoreboard';
+
+// CSS
+import './css/style.css';
+
+ReactDOM.render(<Scoreboard />, document.getElementById('container'));
